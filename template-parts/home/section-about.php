f3d224a9270af658563c7e5abc0d35f8b85f6a07
pedro-digid <pedro@dig.id>
2022-11-09 14:25:56 +0000

    up

--- template-parts/home/section-about.php
+++ template-parts/home/section-about.php
@@ -1,9 +1,16 @@
-<section id="section-about" class="section section-about">
+<section id="about" class="section section-about">
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<h1 class="section__title">About title here</h1>
-				<p class="section__description">Nullam a ultricies nisl, id variu.Nulla tristique vitae augue pellentesque fringilla.</p>
+				<h1 class="section__title section-about__title"><?php the_field( 'about_title' ); ?></h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 col-md-6">
+				<?php the_field( 'about_description_block_1' ); ?>
+			</div>
+			<div class="col-12 col-md-6">
+				<?php the_field( 'about_description_block_2' ); ?>
 			</div>
 		</div>
 	</div>
--- template-parts/home/section-hero.php
+++ template-parts/home/section-hero.php
@@ -1,10 +1,10 @@
-<section class="section section-hero" style="background-color: #464646; background: linear-gradient( rgba(15, 32, 28, 0.7), rgba(15, 32, 28, 0.7) ) , url( <?php the_post_thumbnail_url( 'full' ); ?> ); background-position: center; background-repeat: no-repeat; background-size: cover;">
+<section class="section section-hero" style="background-color: #464646; background: linear-gradient( rgba(15, 32, 28, 0.7), rgba(15, 32, 28, 0.7) ) , url( <?php the_field( 'hero_image' ); ?> ); background-position: center; background-repeat: no-repeat; background-size: cover;">
 	<div class="container">
 		<div class="row">
 			<div class="col-12 d-flex flex-column justify-content-center align-items-center">
-				<h1 class="section__title section-hero__title text-center">Insert hero title here</h1>
-				<p class="section__description section-hero__description text-center">Nullam a ultricies nisl, id variu.Nulla tristique vitae augue pellentesque fringilla.</p>
-				<a href="#section-about" class="section-hero__btn"><svg xmlns="http://www.w3.org/2000/svg" width="30.635" height="19.523" viewBox="0 0 30.635 19.523"><defs><style>.a{fill:none;stroke:#202020;stroke-linecap:round;stroke-linejoin:round;stroke-width:4px;}</style></defs><g transform="translate(-1078.1 -624.391)"><path class="a" d="M.051,17.668,2,.006Z" transform="translate(1094.835 639.676) rotate(135)"/><path class="a" d="M1.947,17.662,0,0Z" transform="translate(1093.417 641.085) rotate(-135)"/></g></svg></a>
+				<h1 class="section__title section-hero__title text-center"><?php the_field( 'hero_title' ); ?></h1>
+				<p class="section__description section-hero__description text-center"><?php the_field( 'hero_description' ); ?></p>
+				<a href="#about" class="section-hero__btn"><svg xmlns="http://www.w3.org/2000/svg" width="30.6" height="19.5" style="enable-background:new 0 0 30.6 19.5" xml:space="preserve"><path style="fill:#202020" d="M15.3 18.7c-.6 0-1.2-.3-1.6-.7L2.6 4.1c-.3-.4-.4-.8-.4-1.3 0-.6.3-1.2.7-1.6.9-.7 2.1-.6 2.8.3l9.5 11.9 9.6-11.9c.7-.9 2-1 2.8-.3.5.4.7 1 .7 1.6 0 .4-.1.9-.4 1.2l-11 13.9c-.1.1-.2.3-.4.4-.3.3-.7.4-1.2.4z"/></svg></a>
 			</div>
 		</div>
 	</div>
