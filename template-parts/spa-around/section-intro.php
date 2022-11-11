381ac6fbccd09a19aab6efd64e03fee3849de901
pedro-digid <pedro@dig.id>
2022-11-11 11:03:08 +0000

    up

--- template-parts/spa-around/section-intro.php
+++ template-parts/spa-around/section-intro.php
@@ -1,9 +1,18 @@
-<section class="section section-intro" style="padding: 150px;">
+<section class="section section-intro">
 	<div class="container">
 		<div class="row">
-			<div class="col-12">
-				<p class="section__description">Nullam a ultricies nisl, id variu.Nulla tristique vitae augue pellentesque fringilla.</p>
+			<div class="col-12 col-md-6 d-flex justify-content-between align-items-center">
+				<img class="pe-5" src="<?php echo esc_url( get_template_directory_uri() . '/assets/imgs/icon-spa-around.svg' ); ?>" alt="spa around icon">
+				<p class="section__description mb-0">Nullam a ultricies nisl, id variu.Nulla tristique vitae augue pellentesque fringilla.</p>
 			</div>
 		</div>
 	</div>
 </section>
+<div class="container separator">
+	<div class="row">
+		<div class="col-12">
+			<hr class="separator-line">
+		</div>
+	</div>
+</div>
+
