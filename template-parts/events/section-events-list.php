5e3646ac38aebb2a1e43e18cecbeb978b819227f
Daniel-Digid <daniel@dig.id>
2022-11-15 16:16:06 +0000

    Dev

--- template-parts/events/section-events-list.php
+++ template-parts/events/section-events-list.php
@@ -1,52 +1,25 @@
 <section class="section section-list" style="padding: 30px 0 50px 0;">
 	<div class="container">
-		<div class="row">
+        <div class="row">
             <div class="col-12">
-                <p class="offer__filter-title"><?php _e('Filter', 'spa-around') ?></p>
+                <p class="event__filter-title"><?php _e('Filter', 'spa-around') ?></p>
             </div>
         </div>
-		<div class="row">
-			<div class="col-12">
-                <p class="offer__filter-name"><?php _e('Category', 'spa-around') ?></p>
-            </div>
-			<div class="col-12">
-				<div class="button-group offer__filter-button-group">
-					<button class="offer__filter-button" data-filter="*">All</button>
-					<?php 
-					$offer_allterms = get_terms( 'category', array('hide_empty' => true) ); 
-					if ( $offer_allterms && ! is_wp_error( $offer_allterms ) ) :
-						foreach ( $offer_allterms as $offer_allterm ) :
-							$offer_category_slug = $offer_allterm->slug;
-							$offer_category = $offer_allterm->name;
-							echo '<button class="offer__filter-button" data-filter=".' . esc_attr( $offer_category_slug ) . '">' . esc_html( $offer_category ) . '</button>';
-						endforeach;
-					endif;
-					?>
-				</div>
-			</div>
-		</div>
-		<div class="row grid-offer">
+		<div class="row grid-event">
 			<?php
-			$offer_query_args = array(
-				'post_type' => 'offer',
+			$event_query_args = array(
+				'post_type' => 'events',
 				'nopaging'  => true,
 				'orderby'   => 'date',
 				'order'     => 'ASC',
 			);
-			$offer_query = new WP_Query( $offer_query_args );
-			if ( $offer_query->have_posts() ) :
-				while ( $offer_query->have_posts() ) :
-				$offer_query->the_post();
+			$event_query = new WP_Query( $event_query_args );
+			if ( $event_query->have_posts() ) :
+				while ( $event_query->have_posts() ) :
+				$event_query->the_post();
 				?>
-				<?php $offer_terms = get_the_terms( $post->ID, 'category' ); ?>
-				<?php $offer_categorys = array();
-						if ( $offer_terms && ! is_wp_error( $offer_terms ) ) :
-						foreach ( $offer_terms as $offer_term ) {
-							$offer_categorys[] = $offer_term->slug;
-						} 
-						$offer_category = join( " ", $offer_categorys ); ?>
-				<article class="col-12 col-md-4 grid-offer-item <?php echo esc_html( $offer_category ); ?>">
-					<?php endif; ?>
+				
+				<article class="col-12 col-md-4 grid-event-item">
 					<a href="<?php the_permalink(); ?>" class="card card-link">
 						<figure>
 							<?php if ( has_post_thumbnail() ) : ?>
@@ -69,13 +42,8 @@
 <script type="text/javascript">
 	(function( $ ) {
 		$(document).on( 'ready', function() {
-			$('.offer__filter-button-group').on( 'click', 'button', function() {
-			var filterValue = $(this).attr('data-filter');
-			$grid.isotope({ filter: filterValue });
-			});
-
-			var $grid = $(".grid-offer").isotope({
-				itemSelector: '.grid-offer-item',
+			var $grid = $(".grid-event").isotope({
+				itemSelector: '.grid-event-item',
 				layoutMode: 'fitRows'
 			});
 		});
