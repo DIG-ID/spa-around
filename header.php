f67264cdab7c14bf3f851d0b529e7b32adaa4d9d
peter <pedro@dig.id>
2022-12-02 16:19:45 +0000

    Update header.php

--- header.php
+++ header.php
@@ -6,6 +6,14 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<?php wp_head(); ?>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-P1BQH157NF"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'G-P1BQH157NF');
+		</script>
 	</head>
 
 	<body <?php body_class(); ?>>
