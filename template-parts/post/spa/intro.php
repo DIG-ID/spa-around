d7872caf465ae04b5a04f7afee6b38f3327a2f78
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2022-12-09 16:45:16 +0000

    up

--- template-parts/post/spa/intro.php
+++ template-parts/post/spa/intro.php
@@ -19,16 +19,16 @@
 									<?php echo do_shortcode( $booking_form ); ?>
 								</div>
 								<div class="modal-footer">
-									<div class="alert alert-warning d-flex align-items-center" role="alert">
+									<div class="alert alert-info d-flex align-items-center" role="alert">
 										<svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 me-2" viewBox="0 0 512 512" role="img" aria-label="Warning:"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zm32 224c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z"/></svg>
 										<p class="modal-footer__note">
-											<?php _e( 'Bitte beachten Sie, dass pro Person ein separater Termin gebucht werden muss.', 'digid' ); ?>
+											<?php _e( 'Please note that a separate appointment must be booked per person.', 'digid' ); ?>
 										</p>
 									</div>
-									<div class="alert alert-warning d-flex align-items-center" role="alert">
+									<div class="alert alert-info d-flex align-items-center" role="alert">
 										<svg xmlns="http://www.w3.org/2000/svg" class="flex-shrink-0 me-2" viewBox="0 0 512 512" role="img" aria-label="Warning:"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zm32 224c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z"/></svg>
 										<p class="modal-footer__note">
-											<?php _e( 'Vor Ort wirst du an der jeweiligen Rezeption gebeten, dich mit der Hotelschlüsselkarte mit Namen oder Gästekarte zu identifizieren.', 'digid' ); ?>
+											<?php _e( 'On site, you will be asked at the respective reception to identify yourself with the hotel key card with name or guest card.', 'digid' ); ?>
 										</p>
 									</div>
 								</div>
